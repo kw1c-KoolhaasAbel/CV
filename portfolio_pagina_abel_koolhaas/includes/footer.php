3af85c6e06df2fa2dc50d82403c98b3971d4d9e0
kw1c-KoolhaasAbel <abel.koolhaas@edu-kw1c.nl>
2025-03-29 03:37:47 +0100

    versie 0.5
    stijling ongeveer compleet
    
    to do back end?

---
+++ portfolio_pagina_abel_koolhaas/includes/footer.php
@@ -0,0 +1,8 @@
+<footer>
+    e-mail: Abel.koolhaas@edu-kw1c.nl
+    <br>
+    linked-in van: 
+    <a href="https://www.linkedin.com/in/abel-koolhaas-609192359/" target="_blank">Abel Koolhaas</a>
+    
+</footer>
+</html>
---
+++ portfolio_pagina_abel_koolhaas/index.php
@@ -0,0 +1,83 @@
+<?php
+include 'includes/header.php';
+?>
+<title>home pagina</title>
+<body>
+    <main>
+        <article id='inleiding'>
+            <div id='over_mijzelf'>
+                <h2>Kort over mijzelf</h2>
+                <img src="img/fotovanmij1.jpg" alt="mooie foto van Abel Koolhaas">
+                <p>
+                    Ik ben gedreven, enthousiast en een doorzetter,
+                    die altijd zoekt om iets nieuws te leren of uitdagingen.
+                    Ik zou zo mezelf omschrijven.
+                    Waarneer ik mensen beetje heb leren kennen kan ik
+                    fantastisch met anderen mensen samen werken,
+                    daarnaast kan ik zorgvuldig werken
+                    en ben goed met data.
+                </p>
+            </div>
+            <div id='talen_kennis'>
+                <h3>Welke programeer talen kan ik?</h3>
+                <ul>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>Basis van JS</li>
+                    <li>Basis van PHP</li>
+                    <li>Basis van PYTHON</li>
+                </ul>
+            </div>
+        </article>
+        <article id='werk_stage'>
+            <h2>werkervaring/stages</h2>
+            <div id=stage>
+                <h3>stage</h3>
+                <p>
+                    Ik heb bij het bedrijf Autobedrijf van Arkel in Opijnen stage gelopen.
+                    daar moest ik voorall basis dingen doen met auto's, zoals
+                    banden, olie vervangen en auto's schoon maken/stickers eraf maken.
+                </p>
+                <img src="img/werk/autobedrijf_van_Arkel.jpg" alt=" bedrijf Autobedrijf van Arkel">
+            </div>
+            <div id=werkervaring>
+                <h3>werkervaring</h3>
+                <p>
+                    Ik werk nu bijna 2 jaar bij macdonalds Waardenburg.
+                    daar zijn ze heel blij met mij en daar doe ik eigenlijk van alles.
+                </p>
+                <img src="img/werk/werkplek.jpg" alt="macdonalds waardenburg">
+            </div>
+        </article>
+        <article id='opleidingen'>
+            <figure>
+                <img src="img/scholen/kw1c.jpg" alt="kw1c">
+                <figcaption class=foto_links>
+                    Hier zit ik nu op school en heb het eerste jaar van software develepment afgerond.
+                    <br>
+                    2024-heden
+                </figcaption>
+            </figure>
+            <figure>
+                <figcaption id=foto_rechts>
+                    Op het kwc heb ik havo gedaan en uiteindelijk mavo afgerondt.
+                    <br>
+                    2019-2024
+                </figcaption>
+                <img src="img/scholen/kwc.avif" alt="kwc">
+            </figure>
+            <figure>
+                <img src="img/scholen/kastanjepoort.avif" alt="kastanjepoort">
+                <figcaption class=foto_links>
+                    op het kestanjepoort heb ik op de basisschool gezeten.
+                    jammergenoeg is these school wel gesloten.
+                    <br>
+                    2011-2019
+                </figcaption>
+            </figure>
+        </article>
+    </main>
+</body>
+<?php
+include 'includes/footer.php';
+?>
