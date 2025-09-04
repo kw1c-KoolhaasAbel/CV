94e83cb40d7125bd3ea12ff9eeae5f9b2e6c2302
kw1c-KoolhaasAbel <abel.koolhaas@edu-kw1c.nl>
2025-09-04 09:11:01 +0200

    Delete portfolio_pagina_abel_koolhaas directory

--- portfolio_pagina_abel_koolhaas/includes/footer.php
+++
@@ -1,8 +0,0 @@
-<footer>
-    e-mail: Abel.koolhaas@edu-kw1c.nl
-    <br>
-    linked-in van: 
-    <a href="https://www.linkedin.com/in/abel-koolhaas-609192359/" target="_blank">Abel Koolhaas</a>
-    
-</footer>
-</html>
